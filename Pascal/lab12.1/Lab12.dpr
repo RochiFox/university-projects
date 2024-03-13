028ffbf4c7870bff23be87fa6b7bd4b356b99963
RochiFox <rochithefox@gmail.com>
2024-03-13 19:25:54 +0200

    Added: added lab 12.1, 13.1

---
+++ Pascal/lab12.1/Lab12.dpr
@@ -0,0 +1,30 @@
+﻿program lab12;
+
+uses
+  System.SysUtils,
+  UnitVector in 'UnitVector.pas';
+
+Var
+  v1, v2: Vector;
+
+begin
+    writeln('Create vectors');
+
+    v1 := Vector.Create;
+    v2 := Vector.Create;
+
+    v1.Init('A');
+    v2.Init('D');
+
+    v1.ShowVec('A'); // Виведення вектора А
+    v2.ShowVec('B'); // Виведення вектора В
+
+    v1.Scalar(v2); // Обчислення скалярного добутку векторів
+
+    v1.Free; // Звільнення пам'яті використання деструктору
+    v2.Free; // Звільнення пам'яті використання деструктору
+
+    readln;
+    readln;
+end.
+
---
+++ Pascal/lab12.1/UnitVector.pas
@@ -0,0 +1,68 @@
+﻿unit UnitVector;
+
+interface
+
+type
+  Mas = array [1..10] of Integer;
+
+  Vector = class
+    private
+      n: Integer; // Розмірність вектору
+      vec: Mas; // Значення елементів вектора
+      name: Char; // Ім'я вектора
+
+    public
+      procedure Init(ch: Char);
+      procedure ShowVec(ch: Char);
+      procedure Scalar(v: Vector);
+  end;
+
+implementation
+
+// Ініціалізація вектора
+procedure Vector.Init(ch: Char);
+var
+  i: Integer;
+begin
+  name := ch;
+  writeln('Input the number of vector ', name, ' components');
+  readln(n);
+  writeln('Input ', n, ' components:');
+  for i := 1 to n do
+    read(vec[i]);
+end;
+
+// Вивід вектора
+procedure Vector.ShowVec(ch: Char);
+var
+  i: Integer;
+begin
+  name := ch;
+  writeln('Vector ', name, ':');
+  for i := 1 to n do
+    write(vec[i], ' ');
+  writeln;
+end;
+
+// Обчислення скалярного добутку вектора
+procedure Vector.Scalar(v: Vector);
+var
+  sum, i: Integer;
+begin
+  if n <> v.n then
+  begin
+    writeln('Vector lengths are different');
+    readln;
+    exit;
+  end
+  else
+  begin
+    sum := 0;
+    for i := 1 to n do
+      sum := sum + vec[i] * v.vec[i];
+    writeln('Scalar product = ', sum);
+  end;
+end;
+
+end.
+
